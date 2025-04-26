360b4d105538a14af20ca5c4fe2cb2a25ca6e9cb
goyou2845 <goyou2845@gmail.com>
2025-04-26 19:14:09 +0900

    Fix: change default port to 5000 for Render

--- app.py
+++ app.py
@@ -157,5 +157,6 @@ def download(filename):
     return send_file(filepath, as_attachment=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))  # デフォルト5000に
     app.run(host="0.0.0.0", port=port)
+
